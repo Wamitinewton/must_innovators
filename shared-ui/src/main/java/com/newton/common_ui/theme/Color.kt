53b3e18cb1fe26e82b4e6ddce1c2fa7cdaf7878b
newton <wamitinewton@gmail.com>
2025-04-02 09:38:21 -0400

    updated theme colors for light theme support

--- shared-ui/src/main/java/com/newton/common_ui/theme/Color.kt
+++ shared-ui/src/main/java/com/newton/common_ui/theme/Color.kt
@@ -3,5 +3,5 @@ package com.newton.common_ui.theme
 import androidx.compose.ui.graphics.Color
 
 val PurpleGrey40 = Color(0xFF625b71)
-val lightGrayBackgroundColor = Color(0xffF5F5F5)
-val blackColor = Color(0xff20201E)
+val lightGrayBackgroundColor = Color(0xFFF8F8F8)
+val blackColor = Color(0xFF121212)
