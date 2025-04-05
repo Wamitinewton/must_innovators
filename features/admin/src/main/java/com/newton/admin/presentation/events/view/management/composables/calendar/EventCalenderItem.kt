2192ba24887e885532acc487af4965bc9224cf01
newton <wamitinewton@gmail.com>
2025-04-05 11:24:33 -0400

    spotless

--- features/admin/src/main/java/com/newton/admin/presentation/events/view/management/composables/calendar/EventCalenderItem.kt
+++ features/admin/src/main/java/com/newton/admin/presentation/events/view/management/composables/calendar/EventCalenderItem.kt
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2025 Meru Science Innovators Club
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Meru Science Innovators Club.
+ * You shall not disclose such confidential information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with Meru Science Innovators Club.
+ *
+ * Unauthorized copying of this file, via any medium, is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ * NO WARRANTY: This software is provided "as is" without warranty of any kind,
+ * either express or implied, including but not limited to the implied warranties
+ * of merchantability and fitness for a particular purpose.
+ */
 package com.newton.admin.presentation.events.view.management.composables.calendar
 
 import androidx.compose.foundation.layout.*
