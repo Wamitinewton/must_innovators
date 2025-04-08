c781dcb0e2028841e0cc6fc40368e96efc38ffd0
newton <wamitinewton@gmail.com>
2025-04-08 13:26:47 -0400

    added spotless to files

--- features/testimonials/src/main/java/com/newton/testimonials/presentation/view/allTestimonials/RatingSummary.kt
+++ features/testimonials/src/main/java/com/newton/testimonials/presentation/view/allTestimonials/RatingSummary.kt
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2025 Meru Science Innovators Club
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Meru Science Innovators Club.
+ * You shall not disclose such confidential information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with Meru Science Innovators Club.
+ *
+ * Unauthorized copying of this file, via any medium, is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ * NO WARRANTY: This software is provided "as is" without warranty of any kind,
+ * either express or implied, including but not limited to the implied warranties
+ * of merchantability and fitness for a particular purpose.
+ */
 package com.newton.testimonials.presentation.view.allTestimonials
 
 import androidx.compose.foundation.layout.*
@@ -76,7 +92,7 @@ fun RatingSummary(testimonials: List<TestimonialsData>) {
                     .padding(start = 16.dp)
             ) {
                 for (i in 5 downTo 1) {
-                    val count = ratingCounts[i-1]
+                    val count = ratingCounts[i - 1]
                     val percentage = count.toFloat() / maxCount.toFloat()
 
                     Row(
@@ -116,8 +132,8 @@ fun RatingSummary(testimonials: List<TestimonialsData>) {
                     if (i > 1) {
                         Spacer(modifier = Modifier.height(4.dp))
                     }
-                    }
                 }
             }
         }
     }
+}
